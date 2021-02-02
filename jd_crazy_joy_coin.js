662a19a8e94b981338b872b29b1938e24febc789
Zhibo <z.io@qq.com>
2021-02-02 19:01:26 +0800

    Update jd_crazy_joy_coin.js

--- jd_crazy_joy_coin.js
+++ jd_crazy_joy_coin.js
@@ -161,8 +161,7 @@ if ($.isNode()) {
     }, 8000);
   }
 
-  while (count < 400) {
-//   while (true) {
+  while (true) {
     count++
     console.log(`============开始第${count}次挂机=============`)
     for (let i = 0; i < cookiesArr.length; i++) {
